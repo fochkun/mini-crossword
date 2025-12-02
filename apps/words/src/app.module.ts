02289fe34bbbbf071442562ce7c0c970b70011ac
fochkun <dvchervakov@gmail.com>
2025-12-02 16:15:53 +0300

    link to db, add word entity

--- apps/words/src/app.module.ts
+++ apps/words/src/app.module.ts
@@ -1,9 +1,49 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { WordEntity } from './entities/word.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const url = configService.get<string>('POSTGRES_HOST');
+        const username =
+          configService.get<string>('POSTGRES_USERNAME') || 'user';
+        const password =
+          configService.get<string>('POSTGRES_PASSWORD') || 'password';
+        const port = Number(configService.get<string>('POSTGRES_PORT') || 5432);
+        console.log('url is', url);
+        console.log('username is', username);
+        console.log('password is', password);
+        console.log('port is', port);
+
+        return {
+          host: url,
+          database: 'words_db',
+          type: 'postgres',
+          // url,
+          port,
+          username,
+          password,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          migrations: [__dirname + '/../migration/*.ts'],
+          migrationsRun: false,
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([
+      WordEntity,
+      // ContragentEntity,
+      // ContractEntity,
+      // DistrictEntity,
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/words/src/entities/word.entity.ts
@@ -0,0 +1,46 @@
+// src/entities/Word.entity.ts
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('words')
+export class WordEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 50, unique: true })
+  word: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  clue: string;
+
+  @Column({ type: 'int' })
+  length: number;
+
+  @Column({ type: 'char', length: 1, nullable: true })
+  letter_1: string | null;
+
+  @Column({ type: 'char', length: 1, nullable: true })
+  letter_2: string | null;
+
+  @Column({ type: 'char', length: 1, nullable: true })
+  letter_3: string | null;
+
+  @Column({ type: 'char', length: 1, nullable: true })
+  letter_4: string | null;
+
+  @Column({ type: 'char', length: 1, nullable: true })
+  letter_5: string | null;
+
+  @Column({ type: 'char', length: 1, nullable: true })
+  letter_6: string | null;
+
+  @Column({ type: 'char', length: 1, nullable: true })
+  letter_7: string | null;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
