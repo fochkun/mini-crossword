4f19451c6ac6d777560308ad9fdf10b87ecb7c3b
fochkun <dvchervakov@gmail.com>
2025-12-02 16:15:16 +0300

    add nodemon

--- apps/storage/src/app.controller.ts
+++ apps/storage/src/app.controller.ts
@@ -3,10 +3,17 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+    console.log('start controller');
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('health')
+  healthCheck() {
+    return { status: 'ok', devMode: true, timestamp: new Date() };
   }
 }
